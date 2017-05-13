44e0df24529161e98ad3ef8730f654b4d891a154
Bintang candra ayu srikandi <Bintang candra ayu srikandi>
2017-05-13 11:36:43 +0700

    add css baru

--- login.php
+++ login.php
@@ -1,7 +1,19 @@
 <?php session_start(); ?>
 <html>
 <head>
-	<title>Login</title>
+<meta charset="utf-8">
+    <title>Login - Toko Sembako</title>
+ <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+     <link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet"> <!-- 3 KB -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script> <!-- 16 KB -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <a href="index.php" class="navbar-brand">Toko Sembako</a>
 </head>
 
 <body>
